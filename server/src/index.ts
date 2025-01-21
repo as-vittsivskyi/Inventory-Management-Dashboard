8901456dfdec9591bd217a6ac0b096ed949eb664
as-vittsivskyi <asvittcivskyi04@gmail.com>
2025-01-21 23:33:23 +0200

    create api hooks for get and create pages

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 // ROUTE IMPORTS
 import dashboardRoutes from "./routes/dashboardRoutes"
-
+import productRoutes from "./routes/productRoutes"
 // CONFIGURATIONS
 dotenv.config();
 const app = express();
@@ -20,6 +20,7 @@ app.use(cors());
 
 // ROUTES
 app.use("/dashboard", dashboardRoutes) // http://localhost:8000/dashboard
+app.use("/products", productRoutes) // http://localhost:8000/products
 
 // SERVER
 const port = process.env.PORT || 3001;
